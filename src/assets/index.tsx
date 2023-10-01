2d16b9f2bd9bea033e0fae6f9d31f16b694cccf4
nguyenhung <antialonedn1@gmail.com>
2023-10-01 15:41:31 +0700

    nav-bar lg-sm

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,10 +1,109 @@
 import logo from './coffeestyle_logo.png';
 import cart_icon from './cart_icon.png';
 import menu_icon from './menu_icon.png';
+import close_icon from './close_icon.png';
+import empty_cart_icon from './empty_cart_icon.png';
+import header_pic from './header_pic.jpg';
+import pic_1 from './pic_1.jpg';
+import pic_2 from './pic_2.jpg';
+import pic_3 from './pic_3.jpg';
+import pic_4 from './pic_4.jpg';
+import pic_5 from './pic_5.jpg';
+import pic_6 from './pic_6.jpg';
+import pic_7 from './pic_7.jpg';
+import pic_8 from './pic_8.jpg';
+import pic_9 from './pic_9.jpg';
+import pic_10 from './pic_10.jpg';
+import pic_11 from './pic_11.jpg';
+import pic_12 from './pic_12.jpg';
+import pic_13 from './pic_13.jpg';
+import pic_14 from './pic_14.jpg';
+import pic_15 from './pic_15.jpg';
+import pic_16 from './pic_16.jpg';
+import pic_17 from './pic_17.jpg';
+import pic_18 from './pic_18.jpg';
+import pic_19 from './pic_19.jpg';
+import pic_20 from './pic_20.jpg';
+import pic_21 from './pic_21.jpg';
+import pic_22 from './pic_22.jpg';
+import pic_23 from './pic_23.jpg';
+import pic_24 from './pic_24.jpg';
+import author_1 from './authors/author_1.jpg';
+import author_2 from './authors/author_2.jpg';
+import author_3 from './authors/author_3.jpg';
+import about_image_1 from './about_image_1.jpg';
+import about_image_2 from './about_image_2.jpg';
+import about_image_3 from './about_image_3.jpg';
+import paralax_image_2 from './paralax_image_2.jpg';
+import contact_image_1 from './contact_image_1.jpg';
+import contact_image_2 from './contact_image_2.jpg';
+import check_col from './check_col.png';
+import check_row from './check_row.png';
+import not_found from './not_found.svg';
+import no_image from './no_image.png';
+import diamond from './diamond.png';
+import light_bulb from './light_bulb.png';
+import premium_image from './premium_image.jpg';
+// themes
+import flutter_bg from './flutter_bg.png';
+import nextjs_bg from './nextjs_bg.jpg';
+import nuxtjs_bg from './nuxtjs_bg.jpg';
+import remixjs_bg from './remixjs_bg.jpg';
+
 const images = {
     logo,
     cart_icon,
     menu_icon,
+    close_icon,
+    empty_cart_icon,
+    header_pic,
+    pic_1,
+    pic_2,
+    pic_3,
+    pic_4,
+    pic_5,
+    pic_6,
+    pic_7,
+    pic_8,
+    pic_9,
+    pic_10,
+    pic_11,
+    pic_12,
+    pic_13,
+    pic_14,
+    pic_15,
+    pic_16,
+    pic_17,
+    pic_18,
+    pic_19,
+    pic_20,
+    pic_21,
+    pic_22,
+    pic_23,
+    pic_24,
+    diamond,
+    light_bulb,
+    premium_image,
+    author_1,
+    author_2,
+    author_3,
+    about_image_1,
+    about_image_2,
+    about_image_3,
+    paralax_image_2,
+    not_found,
+    contact_image_1,
+    contact_image_2,
+    check_col,
+    check_row,
+    no_image,
+    // themes
+    flutter_bg,
+    nextjs_bg,
+    nuxtjs_bg,
+    remixjs_bg,
 };
+
+export const theme_backgrounds = {};
 
 export default images;
